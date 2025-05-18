3f83dae565b103eeef86966a9bf44059d31f0f87
Kiruti-Tech_Git <kirutitech@gmail.com>
2025-05-18 15:48:17 +0300

    auth UI -- Login/Sign up

--- src/app/(protected)/(tabs)/index.tsx
+++ src/app/(protected)/(tabs)/index.tsx
@@ -1,19 +1,27 @@
-import { dummyPosts } from '@/dummyData';
-import { StatusBar } from 'expo-status-bar';
-import { FlatList, StyleSheet, Text, View } from 'react-native';
-import PostListItem from '@/components/PostListItem';
-import { Link } from 'expo-router';
+import { dummyPosts } from "@/dummyData";
+import { StatusBar } from "expo-status-bar";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import PostListItem from "@/components/PostListItem";
+import { Link } from "expo-router";
 
 export default function HomeScreen() {
   return (
     <FlatList
       data={dummyPosts}
-      renderItem={({item}) => <PostListItem post={item} />}
+      renderItem={({ item }) => <PostListItem post={item} />}
       ListHeaderComponent={() => (
-        <Link href='/new' className='text-blue-500 p-4 text-center text-3xl'>
-          New Post
-        </Link>
+        <>
+          <Link href="/new" className="text-blue-500 p-4 text-center text-3xl">
+            New Post
+          </Link>
+          <Link
+            href="/login"
+            className="text-blue-500 p-4 text-center text-3xl"
+          >
+            Login
+          </Link>
+        </>
       )}
-      />
+    />
   );
 }
